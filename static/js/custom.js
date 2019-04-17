3f62b4aa424fe623addb0880a5b9390baa7ac94f
Ashwath Nanda <ashwath5897@gmail.com>
2019-04-17 12:57:32 +0530

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -19,12 +19,28 @@ function checkForm() {
 		(document.getElementById("q08N")).checked &&
 		(document.getElementById("q09Y")).checked &&
 		(document.getElementById("q10Y")).checked) {
-		(document.getElementById("out")).innerHTML = "Get the subscription";
+		(document.getElementById("out")).innerHTML = "It is adviced that you get the subscription plan as you are using an older version of java and only subscribed members will get critical bug fixes and performance patches. Moreover using components like JRockit, Mission control are liable for subscription.";
 	}
+	
+	else if ((document.getElementById("q01Y")).checked &&
+			(document.getElementById("q02Y")).checked &&
+			(document.getElementById("q03Y")).checked &&
+			(document.getElementById("q04N")).checked &&
+			(document.getElementById("q05Y")).checked &&
+			(document.getElementById("q06N")).checked &&
+			(document.getElementById("q07N")).checked &&
+			(document.getElementById("q08N")).checked &&
+			(document.getElementById("q09Y")).checked &&
+			(document.getElementById("q10N")).checked ) {
+				(document.getElementById("out")).innerHTML = "As you are already using services like Azul,Redhat it would be cost effective if you use the JDK provided by them. These companies are planning to provide long term support for their JDK version.";
+			}
+	
+	
+	
 	else if ((document.getElementById("q01Y")).checked &&
 			(document.getElementById("q03N")).checked &&
 			(document.getElementById("q08N")).checked )	{
-		(document.getElementById("out")).innerHTML = "You don't need the subscription";
+		(document.getElementById("out")).innerHTML = "You don't need the subscriptionas you can keep using OpenJDK and make sure that you are upgrading your JAVA ver every 6 months so that you get patches regularly";
 	}
 	else {
 		(document.getElementById("out")).innerHTML = "Please select all options!!";
