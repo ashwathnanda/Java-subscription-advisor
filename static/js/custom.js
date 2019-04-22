7c89232702559f57d82e6edab35c9120b0ef0763
Ashwath Nanda <ashwath5897@gmail.com>
2019-04-22 15:25:58 +0530

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -19,7 +19,7 @@ function checkForm() {
 		(document.getElementById("q08N")).checked &&
 		(document.getElementById("q09Y")).checked &&
 		(document.getElementById("q10Y")).checked) {
-		(document.getElementById("out")).innerHTML = "It is adviced that you get the subscription plan as you are using an older version of java and only subscribed members will get critical bug fixes and performance patches. Moreover using components like JRockit, Mission control are liable for subscription.";
+		(document.getElementById("out")).innerHTML = "It is adviced that you get the subscription plan as you are using an older version of java and only subscribed members will get critical bug fixes and performance patches. Moreover using components like JRockit, Mission control are liable for subscription. Getting the subscription will also allow you to use oracle products like analytic managment tool. You will also get 24x7 support from oracle.";
 	}
 	
 	else if ((document.getElementById("q01Y")).checked &&
@@ -32,17 +32,18 @@ function checkForm() {
 			(document.getElementById("q08N")).checked &&
 			(document.getElementById("q09Y")).checked &&
 			(document.getElementById("q10N")).checked ) {
-				(document.getElementById("out")).innerHTML = "As you are already using services like Azul,Redhat it would be cost effective if you use the JDK provided by them. These companies are planning to provide long term support for their JDK version.";
+				(document.getElementById("out")).innerHTML = "As you are already using services like Azul,Redhat it would be cost effective if you use the JDK provided by them. These companies are planning to provide long term support for their JDK version. They even have round the clock support from their support department";
 			}
 	
 	
 	
 	else if ((document.getElementById("q01Y")).checked &&
 			(document.getElementById("q03N")).checked &&
-			(document.getElementById("q08N")).checked )	{
-		(document.getElementById("out")).innerHTML = "You don't need the subscriptionas you can keep using OpenJDK and make sure that you are upgrading your JAVA ver every 6 months so that you get patches regularly";
+			(document.getElementById("q07N")).checked &&
+			(document.getElementById("q08Y")).checked )	{
+		(document.getElementById("out")).innerHTML = "You don't need the subscriptionas you can keep using OpenJDK and make sure that you are upgrading your JAVA ver every update cycle so that you get patches regularly. The only downside is that you wont get round the clock support from oracle.";
 	}
 	else {
-		(document.getElementById("out")).innerHTML = "Please select all options!!";
+		(document.getElementById("out")).innerHTML = "Default !! ";
 	}
 }
